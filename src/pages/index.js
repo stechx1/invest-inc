961302fb91781e17fc6415761101a5015b46b897
techx1 <ss.uitech01@gmail.com>
2022-09-07 17:35:50 +0500

    feat(antd-customization): edit background and primary color to our custom color

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Button, Progress, TimePicker } from "antd";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{background : "#171C29"}}>
       <h1>Invest Inc</h1>
       <Button type="primary">Hey there</Button>
     </div>
