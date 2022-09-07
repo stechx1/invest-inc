eaf3d8cc0f33ecebc1fd6b5cc956b626a03d79f2
techx1 <ss.uitech01@gmail.com>
2022-09-07 15:29:35 +0500

    feat(antd-customization): add antd customization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withAntdLess = require('next-plugin-antd-less');
 
-module.exports = nextConfig
+module.exports = withAntdLess({
+  // optional: you can modify antd less variables directly here
+  // modifyVars: { '@primary-color': '#fe009b' },
+  // Or better still you can specify a path to a file 
+  lessVarsFilePath: './styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Button, Progress, TimePicker } from "antd";
 
 export default function Home() {
   return (
     <div>
       <h1>Invest Inc</h1>
+      <Button type="primary">Hey there</Button>
     </div>
   )
 }
